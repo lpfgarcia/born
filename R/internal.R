bf3a36e4f2d7dede98acb9ecf65af28d15cbad62
lpfgarcia <lpfgarcia@gmail.com>
2018-06-11 15:32:06 +0200

    remove

--- R/internal.R
+++
@@ -1,9 +0,0 @@
-sampling <- function(i, data) {
-  sample(setdiff(levels(data$class), data[i,]$class), 1)
-}
-
-random <- function(data, noise) {
-  noise <- intersect(rownames(data), noise)
-  data[noise,]$class <- sapply(noise, sampling, data)
-  return(data)
-}
